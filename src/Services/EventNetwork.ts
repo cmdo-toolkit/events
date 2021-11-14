7fc4e873e7b7668c11b0a16775e8b6746ece5408
Kodemon <dev@kodemon.net>
2021-11-15 02:20:32 +0900

    Add better function names

--- src/Services/EventNetwork.ts
+++ src/Services/EventNetwork.ts
@@ -2,7 +2,24 @@ import type { EventDescriptor } from "../Types/Event";
 import type { StreamNetworkHandler } from "../Types/Stream";
 
 export interface EventNetwork {
-  emit(descriptor: EventDescriptor): Promise<void>;
-  addListener(stream: string, handler: StreamNetworkHandler): void;
-  removeListener(stream: string): void;
+  /**
+   * Push uncomitted descriptors to remote peer.
+   */
+  push(descriptors: EventDescriptor[]): Promise<void>;
+
+  /**
+   * Pull events from the given stream. If a hash is provided we pull
+   * the events greater than the given hash.
+   */
+  pull(stream: string, hash?: string): Promise<EventDescriptor[]>;
+
+  /**
+   * Subscribe to new events for a given stream.
+   */
+  subscribe(stream: string, handler: StreamNetworkHandler): void;
+
+  /**
+   * Unsubscribe from the given stream.
+   */
+  unsubscribe(stream: string): void;
 }
