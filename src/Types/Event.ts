ab9b8aa4e47d9887ca40f077e36aa7f9ca0b2797
Kodemon <dev@kodemon.net>
2021-11-15 02:20:13 +0900

    Minor type adjustments

--- src/Types/Event.ts
+++ src/Types/Event.ts
@@ -7,16 +7,44 @@ export type EventClass<T> = {
 };
 
 export type EventDescriptor<Data extends JSONType = JSONType> = {
+  /**
+   * Stream this descriptor belongs to.
+   */
   stream: string;
+
+  /**
+   * JSON representation of a instanced event. This is the data that
+   * gets used to generate the descriptors hash value.
+   */
   event: EventData<Data>;
+
+  /**
+   * A hashed value of the event value of the descriptor. This is also
+   * used for the top layer when generating a merkle tree.
+   */
+  hash: string;
+
+  /**
+   * Previous event descriptor hash this descriptor is chained to.
+   * When performing a push operation, if the push target latest
+   * descriptor does not match the prevHash value we need to perform
+   * a pull.
+   */
   prevHash?: string;
+
+  /**
+   * A list of heads designating the target position that a peer was
+   * last reporting on this event stream. This allows us to send this
+   * events hash when we want to push events that the peer has not
+   * yet seen.
+   */
+  heads?: string[];
 };
 
 export type EventData<EventData extends JSONType = JSONType> = {
   type: string;
   data: EventData;
   meta: EventMeta;
-  hash: string;
 };
 
 export interface EventMeta {
--- src/Types/Projection.ts
+++ src/Types/Projection.ts
@@ -1,11 +1,21 @@
-import type { Event } from "../Lib/Event";
+import type { Event as BaseEvent } from "../Lib/Event";
 
 export type Options = {
   filter: Filter;
 };
 
 export type Filter = {
+  /**
+   * Hydrated events represents events that are not seen for the first time
+   * in the entierty of its lifetime across all distributed instances.
+   */
   allowHydratedEvents: boolean;
+
+  /**
+   * Outdated events represents events that have already seen the same type
+   * at a later occurence. Eg. If incoming event is older than the latest
+   * local event of the same type, it is considered outdated.
+   */
   allowOutdatedEvents: boolean;
 };
 
@@ -14,13 +24,13 @@ export type State = {
   outdated: boolean;
 };
 
-export type Message<E extends Event = Event> = {
-  event: E;
+export type Message<Event extends BaseEvent = BaseEvent> = {
+  event: Event;
   state: State;
 };
 
-export type Handler<E extends Event = Event> = (event: E) => Promise<void>;
+export type Handler<Event extends BaseEvent = BaseEvent> = (event: Event) => Promise<void>;
 
-export type Listeners = Record<string, Set<Projection> | undefined>;
+export type Listeners = Record<string, Set<ProjectionHandler> | undefined>;
 
-export type Projection<E extends Event = Event> = (event: E, state: State) => Promise<void>;
+export type ProjectionHandler<Event extends BaseEvent = BaseEvent> = (event: Event, state: State) => Promise<void>;
